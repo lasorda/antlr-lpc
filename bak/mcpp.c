007ca4c8e5a5ab470c088428626c1b423821a6f4
panzhihao <panzhihao>
2021-06-16 19:12:02 +0800

    update

---
+++ bak/mcpp.c
@@ -0,0 +1,7 @@
+#include "mcpp_lib.h"
+
+int main(int argc, char *argv[])
+{
+    mcpp_lib_main(argc, argv);
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "LPCListener.h"
 #include "antlr4-runtime.h"
 #include "mcpp_lib.h"
+#include <cstdlib>
 
 using namespace antlrcpptest;
 using namespace antlr4;
@@ -43,8 +44,52 @@ class FunctionListener : public LPCBaseListener
     }
 };
 
+string from_mcpp;
+char buff[102400];
+
+int func_fputc(int c, OUTDEST od)
+{
+    if(od != OUT) return 0;
+    from_mcpp.push_back(c);
+    return 0;
+}
+
+int func_fputs(const char *s, OUTDEST od)
+{
+    if(od != OUT) return 0;
+    from_mcpp.append(s);
+    return 0;
+}
+
+int func_fprintf(OUTDEST od, const char *format, ...)
+{
+    if(od != OUT) return 0;
+    va_list args;
+    va_start(args, format);
+    sprintf(buff, format, args);
+    va_end(args);
+    from_mcpp.append(buff);
+    return 0;
+}
+
 int main(int argc, char const *argv[])
 {
+    int mcpp_argc = 6;
+    char const *mcpp_argv[6];
+
+    mcpp_argv[0] = string("mcpp").c_str();
+    mcpp_argv[1] = string("-I").c_str();
+    mcpp_argv[2] = string("inc").c_str();
+    mcpp_argv[3] = string("-d").c_str();
+    mcpp_argv[4] = string(".").c_str();
+    mcpp_argv[5] = string("cmd/wizard_common.c").c_str();
+
+    mcpp_set_out_func(func_fputc, func_fputs, func_fprintf);
+    //  = {argv[0], "-I", "inc", "-d", ".", "cmd/wizard_common.c"};
+    mcpp_lib_main(mcpp_argc, mcpp_argv);
+
+    cout << from_mcpp;
+    return 0;
     ifstream file("./example.lpc");
     std::string str((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 
